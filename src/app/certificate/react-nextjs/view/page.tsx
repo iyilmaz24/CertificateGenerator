c8613eac622a663231b41f8b0f868c255b8b42af
iyz <iy.usa@outlook.com>
2024-06-03 15:23:28 -0400

    add radix-ui icons, shadcn button, styles, finish /view mvp

--- src/app/certificate/react-nextjs/view/page.tsx
+++ src/app/certificate/react-nextjs/view/page.tsx
@@ -1,19 +1,43 @@
 /* eslint-disable @next/next/no-img-element */
-"use client";
-
+import { Button } from "@/components/ui/button";
 import cert_background from "/public/react-nextjs-cert.png";
-import Image from "next/image";
+import { DownloadIcon, CopyIcon } from "@radix-ui/react-icons";
+
+const iconButtonStyles = "bn hover-bg-light-gray pv2 ph3";
 
 export default function Page() {
   return (
-    <main className="vw-100 vh-100 flex flex-column justify-center items-center">
-      <p>certifcate/react-nextjs/view</p>
-      <div className="w-75vw h-75vh">
-        <img
-          style={{ width: "800px", height: "618px" }}
-          src={cert_background.src}
-          alt="Certificate"
-        />
+    <main className="vw-100 vh-100 flex flex-column items-center">
+      <div
+        className="w-90 h-90 flex flex-column justify-center items-center"
+        style={{ maxWidth: "min(800px, 95%)", height: "auto" }} // critical to scaling/resizing
+      >
+        <img className="ma4 mt5" src={cert_background.src} alt="Certificate" />
+
+        <div className="flex justify-between items-center f7 w-100">
+          <span
+            className="w-100 placeholder br3 tc
+            flex justify-between items-center"
+          >
+            <p className="w-90 overflow-text pl3">
+              http://localhost:3000/certificate/react-nextjs/view
+            </p>
+            <Button asChild>
+              <CopyIcon
+                className={iconButtonStyles}
+                width={"1rem"}
+                height={"1rem"}
+              />
+            </Button>
+            <Button asChild>
+              <DownloadIcon
+                className={iconButtonStyles}
+                width={"1rem"}
+                height={"1rem"}
+              />
+            </Button>
+          </span>
+        </div>
       </div>
     </main>
   );
---
+++ src/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="flex flex-column justify-center items-center">
+      <p>certifcate/react-nextjs</p>
+    </main>
+  );
+}
